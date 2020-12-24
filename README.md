9987155c9b2ef14f41cd873a44c8313586b24cfa
Teloschet <frapgameroficial@hotmail.com>
2020-12-24 10:08:22 -0300

    Equipe CrazzY

---
+++ README.md
@@ -0,0 +1,1 @@
+# equipecrazzy-layout
---
+++ index.php
@@ -0,0 +1,128 @@
+<html>
+    <head>
+        <title>CrazzY Hotel: Crie sua conta, fa&ccedil;a amigos e divirta-se!</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico?v=1.0"/>
+        <meta name="keywords" content="keywords" content="crazzyhotel, crazzy hotel, crazzyhotel.net, equipecrazzy.com,  equipecrazzy, equipe crazzy,  equipecrazy, equipe crazy, lella.la, lella, habbolella, hapixel.info, hapixel.bz,  hapixel,  habbinfo.top, habbinfo.info, habbinfo, habblet.online,  habblet.in, habblet, hablitz.bz, hablitz.info, hablitz, habbo  crazzy, crazzy,  crazzy.net, habbo pirata, top  habbo pirata, crazzy.com, habbo privado, habbo online, habbo com catalogo atualizado,  Habbo, Habbo Pirata, Habbo Hotel, habblet, retro server, habb, rabbo, hotel, Age, Like Hotel, Habbo Privado, Pirata, Privado, Habbo.com.br, habbo.com, habbo.,habbo online, habbo lotado, habbo bom, habbo atualizado, habbo antigo, habbo r63, habbo dino, habbo dinossauro, habbo original, habbo oficial, habblet, habbinfo, habble, habblo, fresh-hotel, crazzy, habbo hotel, virtual, mundo, comunidade virtual, grátis, comunidade, avatar, bate papo, online, jovem, rpg, entre, social, grupos, fóruns, seguro, jogue, jogos, online, amigos, jovens, raros, mobis raros, colecionar, expressão, emblemas, diversão, música, celebridade, visita de famosos, celebridades, mmo, mmorpg, rpg online,habblet, lella, moedas, habbo moedas, habbo gratis, habbo pirata, habbo moedas de graça, habbo atualizado, habbo rpg, sexo habbo, habbo hotel, habbo, games, runescape, world of warcraft, mincraft, keep it real, habbo hotel , virtual, world, redes sociais, jogar, jogos, online, amigos, adolescentes, animais, design de salas, encontros, musica, famosos, mmo, mmorpg,Habbo Pirata, Habbo, Habbo Hotel, habblet, Yuup, Habbinfo, Yuup Hotel, Yuup Online, Yup, Like Hotel, Mania Hotel, yup hotel, Habbo.com.br, habbo.com, habblet, habbinfo, crazzy" />
+        <link rel="shortcut icon" href="https://spacehotel.co/favicon.ico" type="image/vnd.microsoft.icon"/>
+        <link rel="stylesheet" type="text/css" href="assets/css/global.css">
+        <meta name="google-site-verification" content="BTplCS6EeHCIbbGmllBQF5oPs-vRibu3AJUwsFCFqDQ" />
+	    <meta name="google-site-verification" content="T_3wlE0ZuQrllwGJ5o1K3mUqgVlYPbVBbGKigeyF5_k" />
+	    <meta name="msvalidate.01" content="3996C7FA9C1963A7A9C6921CEA753FA4" />
+	    <meta name="description" content="Faça amigos, participe e se destaque! CrazzY Hotel, onde os adolescentes, representados por um personagem pessoal, podem passear, conhecer e conversar.
+"/>
+	    <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="CrazzY Hotel" />
+        <meta name="twitter:description" content="Maak vrienden, doe mee en val op! Virtueel hotel waar tieners, weergegeven door een persoonlijk karaktertje, kunnen rondlopen, elkaar ontmoeten en chatten." />
+        <meta name="twitter:image" content="" />
+        <meta name="twitter:site" content="" />
+        <meta name="twitter:url" content="https://crazzyhotel.net" />
+        <meta name="twitter:widgets:csp" content="on" />
+        <meta property="og:title" content="CrazzY Hotel">
+	    <meta property="og:description" content="Faça parte da maior comunidade online!" />
+	    <meta property="og:type" content="website">
+        <meta property="og:url" content="https://crazzyhotel.net">
+        <meta property="og:image" content="">
+        <meta property="og:site_name" content="CrazzY Hotel">
+        <meta property="og:author" content="Telos">
+        
+        <meta charset="utf-8">
+        
+        
+<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+        
+<link rel="stylesheet" type="text/css" href="fonts/fontawesome-free-5.13.0-web/css/all.min.css">
+
+<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+
+<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+
+<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+
+<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+
+<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+
+<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+
+<link rel="stylesheet" type="text/css" href="css/util.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+        
+        
+    </head>
+    <body>
+<div class="limiter">
+<div class="container-login100">
+<div class="wrap-login100">
+<form class="login100-form validate-form">
+    <div class="logo"></div>
+<span class="login100-form-title p-b-43" style="font-family:Arial;">
+Entrar para continuar
+</span>
+<div class="wrap-input100 validate-input" data-validate="Apenas e-mail válido: crazzyhotel@hotmail.com">
+<input class="input100" type="text" name="email" >
+<span class="focus-input100"></span>
+<span class="label-input100" style="font-family:Arial;">Nick</span>
+</div>
+<div class="wrap-input100 validate-input" data-validate="Requer Senha!">
+<input class="input100" type="password" name="pass" >
+<span class="focus-input100" ></span>
+<span class="label-input100" style="font-family:Arial;">Senha</span>
+</div>
+<div class="flex-sb-m w-full p-t-3 p-b-32">
+<div>
+</div>
+</div>
+<div class="container-login100-form-btn">
+<button class="login100-form-btn" style="font-family:Arial;box-shadow:0 4px 8px 0 rgba(0, 0, 0, 0.2);">
+Entrar
+</button><br><br><br>
+<button class="login100-form-btn" style="background:#ef9524;font-family:Arial;box-shadow:0 4px 8px 0 rgba(0, 0, 0, 0.2);">
+Registre-se
+</button>
+</div>
+<div class="text-center p-t-46 p-b-20">
+<span class="txt1" style="font-family:Arial;font-size:15px;">
+    <b>AS CONTAS ANTIGAS CRIADAS NO LELLA, HAPIXEL, HABBLET, HABLITZ E HABBINFO ENCONTRAM-SE AQUI. NÃO HÁ NECESSIDADE DE REGISTRAR-SE NOVAMENTE!</b>
+</span><br><br>
+<span class="txt2" style="font-family:Arial;">
+CrazzY Hotel 2020. Todos os direitos reservados.<br>
+Powered by Equipe CrazzY & GalaxyServers
+</span>
+</div>
+</form>
+<div class="login100-more" style="background-image: url('/crazzyhotel/43.png');box-shadow:0 4px 8px 0 rgba(0, 0, 0, 0.2);">
+</div>
+</div>
+</div>
+</div>
+
+<script src="vendor/jquery/jquery-3.2.1.min.js" type="d7acfaa9361bd89c499754da-text/javascript"></script>
+
+<script src="vendor/animsition/js/animsition.min.js" type="d7acfaa9361bd89c499754da-text/javascript"></script>
+
+<script src="vendor/bootstrap/js/popper.js" type="d7acfaa9361bd89c499754da-text/javascript"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js" type="af9f60191678616070e9a239-text/javascript"></script>
+
+<script src="vendor/select2/select2.min.js" type="af9f60191678616070e9a239-text/javascript"></script>
+
+<script src="vendor/daterangepicker/moment.min.js" type="af9f60191678616070e9a239-text/javascript"></script>
+<script src="vendor/daterangepicker/daterangepicker.js" type="af9f60191678616070e9a239-text/javascript"></script>
+
+<script src="vendor/countdowntime/countdowntime.js" type="af9f60191678616070e9a239-text/javascript"></script>
+
+<script src="js/main.js" type="af9f60191678616070e9a239-text/javascript"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13" type="d7acfaa9361bd89c499754da-text/javascript"></script>
+<script type="d7acfaa9361bd89c499754da-text/javascript">
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-23581568-13');
+	</script>
+<script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="d7acfaa9361bd89c499754da-|49" defer=""></script></body>
+</html>
